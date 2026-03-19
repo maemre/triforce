623a8f514de2f4f0a01f1b465a3556c13cc0e9de
Mehmet Emre <memre@usfca.edu>
2026-03-19 06:41:24 -0700

    fix the cover test to use the actual result of the optimized program

--- cover-test.sh
+++ cover-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-time RAYON_NUM_THREADS=8 RUST_BACKTRACE=1 cargo run --release --bin search-covers -- triangle=6 triangle=6 triangle=7 2 line-2.json --exact-cover
+time RAYON_NUM_THREADS=8 RUST_BACKTRACE=1 cargo run --release --bin check-covers -- triangle=6 triangle=7 2 line-2.json
 

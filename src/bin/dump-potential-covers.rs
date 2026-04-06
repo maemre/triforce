d23027e4f9c23b85be662b1c31b3669930b8ca8e
Mehmet Emre <memre@usfca.edu>
2026-04-06 13:49:29 -0700

    add a script to dump covers

---
+++ src/bin/dump-potential-covers.rs
@@ -0,0 +1,53 @@
+use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
+use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Instant;
+
+use clap::Parser;
+use triforce::cli::*;
+use triforce::*;
+
+/// Dump the regions R that satisfy:
+///
+/// 1. A ⊆ R ⊆ B
+/// 2. R.len() is divisible by tile_size
+#[derive(Parser, Debug)]
+#[command(name = "check-covers", version)]
+struct Cli {
+    /// The fixed region (nodes required in and allowed in the fixed region)
+    #[arg(required = true)]
+    a: GraphSource,
+
+    /// Nodes allowed in covers of the fixed region
+    #[arg(required = true)]
+    b: GraphSource,
+
+    /// Size of the tiles to tile the graph with
+    #[arg(required = true)]
+    tile_size: usize,
+}
+
+fn main() {
+    env_logger::init();
+    let cli = Cli::parse();
+    let base = read_graph(cli.a, false);
+    let mut allowed_in_covers_r = read_graph(cli.b, true).into_region();
+    allowed_in_covers_r.append(&mut base.clone().into_region());
+    let allowed_in_covers = Graph::from(allowed_in_covers_r);
+
+    assert!(
+        base.len() <= BYTES_IN_COMPACT_REGION * 8,
+        "{} > {}",
+        base.len(),
+        BYTES_IN_COMPACT_REGION * 8
+    );
+
+    let potential_covers = Tiling::potential_covers(&base, &allowed_in_covers, cli.tile_size)
+        .iter()
+        .map(|cr| MaybeRegion::from_region(cr.to_region(&allowed_in_covers)))
+        .collect::<Vec<_>>();
+
+    println!("{}", serde_json::to_string(&potential_covers).unwrap());
+}
